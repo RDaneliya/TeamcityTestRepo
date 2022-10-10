5065c4a5055d632616ef175fda7d496ddcd669ef
Roman Daneliya <daneliyari@sibedge.com>
2022-10-11 01:30:53 +0700

    Initial commit

---
+++ src/main/java/com/example/teamcitytestrepo/TeamcityTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamcitytestrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityTestRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcityTestRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamcitytestrepo/TeamcityTestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamcitytestrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityTestRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
